fa07fef9c0414dc4870047f596dd244fe7486ca6
harshit-506 <supriyamohapatra658@gmail.com>
2024-04-22 22:25:34 +0530

    done with integers

---
+++ chapter2/main1.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var x int // declaration of an integer variable named x
+
+	var y = 10 // declaration and initialization of an integer variable named y
+
+	var z int = 20 // declaration and initialization of an integer variable named z
+
+	// Print the values of x, y, and z
+	fmt.Println("Value of x:", x)
+	fmt.Println("Value of y:", y)
+	fmt.Println("Value of z:", z)
+}
