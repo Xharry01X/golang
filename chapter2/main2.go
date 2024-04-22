7ef9c29797a3904065a8ff7b174d923cb437d9e9
harshit-506 <supriyamohapatra658@gmail.com>
2024-04-22 22:43:31 +0530

    some basic stuff

---
+++ chapter2/main2.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main()  {
+
+	//here it is declared first  and intialize later after using =
+	// var x int
+	// var y int
+
+	// x =4
+	// y=5
+
+
+	  //":=" it is used for decleration and initialization 
+	z :=4
+	h :=5
+
+	fmt.Println("Sum is %d\n",z+h)
+}
+
