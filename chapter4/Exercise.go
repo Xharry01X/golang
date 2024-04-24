f8c55fb5b6a6246c8adb897ee38ede93b694717e
harshit-506 <supriyamohapatra658@gmail.com>
2024-04-24 13:10:34 +0530

    exercise of chapter 2 has done

--- chapter4/Exercise.go
+++ chapter4/Exercise.go
@@ -1,1 +1,37 @@
 package main
+
+
+//question 1: how are integers stored on a computer?
+
+//answer:Integers are stored in binary format computer memory, using a fixed a variable number of bits depending on the system.They can be signed or unsigned,and thier storage might be affected by factors like endianness and mnemory alignment
+
+//question for multiplication two number
+
+import (
+	"fmt"
+)
+
+func main()  {
+	// // var x,y int
+
+	// x=32132
+	// y=42452
+
+	// fmt.Println("Multiplication is ",x*y)
+
+	// name :="Harshit is alive"
+	
+
+	// fmt.Println("Length is now",len(name))
+
+	result := (true && false) || (false && true) || !(false && false)
+	fmt.Println(result)
+
+
+	
+}
+
+//question on string
+
+//String are the characters that are represented inside the quote
+
